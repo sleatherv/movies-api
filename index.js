8f5e8072bd3deabb5b0c58d1410c1957a9bfa4b5
Sleather Vega <sleatherv28@hotmail.com>
2020-11-24 21:31:10 -0500

    Created errorHandler Middleware

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies.js')
 
+const { logErrors, errorHandler } = require('./utils/middlewares/errorHandlers');
+
 //body parser
 app.use(express.json());
 
 moviesApi(app);
 
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(config.port, () => {
     console.log(`Listening http://localhost:${config.port}`);
--- routes/movies.js
+++ routes/movies.js
@@ -13,7 +13,7 @@ function moviesApi(app) {
         const { tags } = req.query;
         try {
             const movies = await movieServices.getMovies({ tags });
-
+            throw new Error('Error getting movies')
             res.status(200).json({
                 data: movies,
                 message: 'movie retrieved'

cbfc9b54d82923ff0f8fcfd1f3296228d7bf9475
Sleather Vega <sleatherv28@hotmail.com>
2020-11-28 20:55:19 -0500

    Instalando Boom

--- utils/middlewares/validationHandler.js
+++ utils/middlewares/validationHandler.js
@@ -2,7 +2,7 @@ function validate() {
     return false;
 }
 
-function validateHandler(schema, check = 'body') {
+function validationHandler(schema, check = 'body') {
     return function (req, res, next) {
         const error = validate(req[check], schema);
 

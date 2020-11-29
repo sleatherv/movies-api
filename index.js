bd83251e9ad5f3b219ce00eb139a35d2a4ac539e
Sleather Vega <sleatherv28@hotmail.com>
2020-11-28 21:09:39 -0500

    Add 404 handler using Boom

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ const app = express();
 const { config } = require('./config/index');
 const moviesApi = require('./routes/movies.js')
 
-const { logErrors, errorHandler } = require('./utils/middlewares/errorHandlers');
+const { logErrors, errorHandler, wrapErrors } = require('./utils/middlewares/errorHandlers');
+const notFoundHandler = require('./utils/middlewares/notFoundHandler');
 
 //body parser
 app.use(express.json());
 
+//routes
 moviesApi(app);
+// catch 404
+app.use(notFoundHandler);
 
+//Error middlewares
 app.use(logErrors);
+app.use(wrapErrors);
 app.use(errorHandler);
 
 app.listen(config.port, () => {

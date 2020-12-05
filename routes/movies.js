34d67f8302024a8c2c0c70854679302c1d4ccab0
Sleather Vega <sleatherv28@hotmail.com>
2020-12-05 18:45:17 -0500

    Added cache control

--- routes/movies.js
+++ routes/movies.js
@@ -7,16 +7,18 @@ const {
     createMovieSchema,
     updateMovieSchema
 } = require('../utils/schemas/movies')
-
+const cacheResponse = require('../utils/cacheResponse');
+const { FIVE_MINUTES_IN_SECONDS, SIXTY_MINUTES_IN_SECONDS } = require('../utils/time');
 const validationHandler = require('../utils/middlewares/validationHandler');
 
 function moviesApi(app) {
     const router = express.Router();
-    app.use("/app/movies", router);
+    app.use("/api/movies", router);
 
     const movieServices = new MoviesService();
 
     router.get("/", async function (req, res, next) {
+        cacheResponse(res, FIVE_MINUTES_IN_SECONDS);
         const { tags } = req.query;
         try {
             const movies = await movieServices.getMovies({ tags });
@@ -30,6 +32,7 @@ function moviesApi(app) {
         }
     });
     router.get("/:movieId", validationHandler({ movieId: movieIdSchema }, 'params'), async function (req, res, next) {
+        cacheResponse(res, SIXTY_MINUTES_IN_SECONDS);
         const { movieId } = req.params;
         try {
             const movies = await movieServices.getMovie({ movieId });

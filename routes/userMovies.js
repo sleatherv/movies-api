cc4322d4f723f7b29212da2588191ed1c25cfe1b
Sleather Vega <sleatherv28@hotmail.com>
2020-12-23 21:30:32 -0500

    Adding the user movie collection

---
+++ routes/userMovies.js
@@ -0,0 +1,28 @@
+const express = require('express');
+
+const UserMoviesService= require('../services/userMovie');
+const validationHandler = require('../utils/middlewares/validationHandler');
+
+const {movieIdSchema} = require('../utils/schemas/movies');
+const {userIdSchema} = require('../utils/schemas/users');
+const {createUserSchema} = require('../utils/schemas/userMovies');
+
+function userMoviesApi(app) {
+    const router = express.Router();
+    app.user('/api/user-movies', router);
+    const userMoviesService = new UserMoviesService();
+
+    router.get('/', validationHandler({userId: userIdSchema}, 'query'),
+    async function(req, res, next) {
+        const {userId} = req.query;
+        try {
+            const userMovies = await userMoviesService.getUserMovie({userId});
+            res.status(200).json({
+                data: userMovies,
+                message: 'user movies listed'
+            })
+        } catch (error) {
+            next(error)
+        }
+    });
+};

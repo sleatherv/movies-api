c4f5f4ee1e498a1fb9e41a2ff4a6962e6b2aec5f
Sleather Vega <sleatherv28@hotmail.com>
2021-01-03 13:16:27 -0500

    Using helmet for security

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
 const app = express();
 
 const { config } = require('./config/index');
@@ -11,6 +12,7 @@ const notFoundHandler = require('./utils/middlewares/notFoundHandler');
 
 //body parser
 app.use(express.json());
+app.use(helmet());
 //enabling CORS
 app.use(cors());
 
